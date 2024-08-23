cb18c3987d3e9f792b300c1d010b02f9a5528b83
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-08-23 18:42:53 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
+import { FaHashnode } from "react-icons/fa6";
+
+import { FiMail } from 'react-icons/fi';
+
+
+const Footer = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<div className='bg-slate-900 p-4' style={{ cursor: 'default' }}>
+			<div className='flex justify-between flex-wrap gap-4'>
+				<p className='text-white text-center w-full sm:w-auto font-light'>© {year} Dadapeer. All rights reserved.</p>
+				<div className='text-white flex justify-around sm:w-[250px] w-full'>
+					
+					<a href="mailto:shaikdadapeer4488@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FiMail className='text-xl' />
+					</a>
+					<a href="https://x.com/Dadu_Dev_" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FaTwitter className='text-xl' />
+					</a>
+					<a href="https://www.linkedin.com/in/dadapeer-shaik-762919273/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<AiFillLinkedin className='text-xl' />
+					</a>
+					<a href="https://github.com/DaduDev" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
+						<FaGithub className='text-xl' />
+					</a>
+					<a href="https://dadudev.hashnode.dev/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FaHashnode className='text-xl' />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
