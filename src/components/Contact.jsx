cf576b29bed273cbf818c78255523482013b3d7e
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-08-27 22:27:27 +0530

    Form Handling is done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,82 +1,15 @@
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
-import { ToastContainer, toast } from 'react-toastify';
+import React from "react";
+import { useForm } from '@formspree/react';
 import 'react-toastify/dist/ReactToastify.css';
-import { EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, EMAIL_JS_PUBLIC_KEY } from "../constants";
 import Footer from "./Footer";
 const Contact = () => {
-	const formRef = useRef();
-	const [form, setForm] = useState({
-		name: "",
-		email: "",
-		message: "",
-	});
+	const [state, handleSubmit] = useForm("xdknbgve");
+	if (state.succeeded) {
+		return (
+			<div className="flex justify-center items-center mt-20">Thank you for your response, we will reach you out soon</div>
+		);
+	}
 
-
-	const [loading, setLoading] = useState(false);
-
-	const handleChange = (e) => {
-		const { target } = e;
-		const { name, value } = target;
-
-		setForm({
-			...form,
-			[name]: value,
-		});
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setLoading(true);
-
-		// cleaning the form data
-		const username = form.name.trim();
-		const user_email = form.email.trim();
-		const user_message = form.message.trim();
-
-		if (username === '' || user_email === '' || user_message === '') {
-			setLoading(false);
-			toast.error("Please fill all the fields.", {
-				position: 'bottom-right',
-			});
-			return;
-		}
-		console.log(username,user_email,user_message)
-
-		emailjs
-			.send(
-				EMAIL_JS_SERVICE_ID,
-				EMAIL_JS_TEMPLATE_ID,
-				{
-					from_name: username,
-					to_name: "Shaik Dadapeer",
-					reply_to: user_email,
-					to_email: "shaikdadapeer4488@gmail.com",
-					message: user_message,
-				},
-				EMAIL_JS_PUBLIC_KEY
-			)
-			.then(
-				() => {
-					setLoading(false);
-					toast.success("Message sent successfully!", {
-						position: 'bottom-right',
-					});
-					setForm({
-						name: "",
-						email: "",
-						message: "",
-					});
-				},
-				(error) => {
-					setLoading(false);
-					console.error(error);
-					toast.error("Uh, oh! Something went wrong. Please try again later.", {
-						position: 'bottom-right',
-					});
-				}
-			);
-	};
 
 	return (
 
@@ -86,7 +19,6 @@ const Contact = () => {
 					<p className='font-light'>REACH OUT TO ME</p>
 					<h2 className='text-5xl font-extrabold mt-2 bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-pink-500'>Contact.</h2>
 					<form
-						ref={formRef}
 						onSubmit={handleSubmit}
 						className='mt-12 flex flex-col gap-8'
 					>
@@ -95,8 +27,6 @@ const Contact = () => {
 							<input
 								type='text'
 								name='name'
-								value={form.name}
-								onChange={handleChange}
 								placeholder="Enter your name"
 								className='py-4 px-6 rounded-lg outline-none border-none font-medium bg-gray-900'
 								required
@@ -107,8 +37,6 @@ const Contact = () => {
 							<input
 								type='email'
 								name='email'
-								value={form.email}
-								onChange={handleChange}
 								placeholder="Ex:abc@gmail.com"
 								className='py-4 px-6 rounded-lg  font-medium bg-gray-900'
 								required
@@ -119,8 +47,6 @@ const Contact = () => {
 							<textarea
 								rows={7}
 								name='message'
-								value={form.message}
-								onChange={handleChange}
 								placeholder='Do you have anything to say?'
 								className='py-4 px-6 rounded-lg outline-none border-none font-medium bg-gray-900'
 								required
@@ -128,10 +54,11 @@ const Contact = () => {
 						</label>
 
 						<button
+							disabled={state.submitting}
 							type='submit'
 							className='pt-3 px-8 rounded-xl outline-none w-fit font-bold shadow-md bg-gray-900'
 						>
-							{loading ? "Sending..." : "Send"}
+							Send
 						</button>
 					</form>
 				</div>
